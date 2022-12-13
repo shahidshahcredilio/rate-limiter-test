04bcc97a9757c58a225fa233a6480efee8d391d0
Shahid Shah <85169976+shahidshahcredilio@users.noreply.github.com>
2022-12-13 13:09:29 +0530

    feat: added route using limiter manager api

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,37 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import { Limiter } from '@adonisjs/limiter/build/services'
 
-Route.get('/', async () => {
+Route.get('/test/using-middleware', async () => {
   return { hello: 'world' }
 }).middleware('throttle:global')
+
+Route.get('/test/using-limiter-manager', async ({ request, response }) => {
+  const throttleKey = request.ip()
+
+  const limiter = Limiter.use({
+    requests: 1,
+    duration: '10 second',
+    blockDuration: '1 min',
+  })
+
+  if (await limiter.isBlocked(throttleKey)) {
+    return response.tooManyRequests('Request attempts exhausted. Please try after some time')
+  }
+
+  try {
+    const username = request.input('username')
+    const password = request.input('password')
+
+    if (username !== 'username' && password !== 'password') {
+      throw 'Incorrect username and password'
+    }
+  } catch (error) {
+    await limiter.increment(throttleKey)
+    return { error: 'An error occured' }
+  }
+
+  await limiter.delete(throttleKey)
+  return { hello: 'world' }
+})
